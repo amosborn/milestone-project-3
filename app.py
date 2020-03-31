cfa24f08362525cd287109dacd4662f4c5fedbc0
amosborn <amosborn@mail.com>
2020-03-31 07:27:43 +0000

    Add edit function to app.py and editreview template

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for, session
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
@@ -47,6 +47,12 @@ def insert_review():
     reviews = mongo.db.reviews
     reviews.insert_one(request.form.to_dict())
     return redirect(url_for('get_reviews'))
+
+
+@app.route('/edit_review/<review_id>')
+def edit_review(review_id):
+    the_review = mongo.db.reviews.find_one({'_id': ObjectId(review_id)})
+    return render_template('editreview.html', review=the_review)
 
 
 if __name__ == '__main__':
